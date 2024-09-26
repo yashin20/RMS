1a0a9c1ab6b2948300338c5ada6d9acaf07da3a4
yashin20 <wlddud8@gmail.com>
2024-09-27 02:22:16 +0900

    #3. 회원(Member) 정보 확인 기능

--- src/main/java/project/RMS/InitData.java
+++ src/main/java/project/RMS/InitData.java
@@ -9,9 +9,12 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import project.RMS.dto.HotelDto;
 import project.RMS.dto.MemberDto;
+import project.RMS.entity.Hotel;
 import project.RMS.entity.Member;
 import project.RMS.entity.MemberRole;
+import project.RMS.service.HotelService;
 import project.RMS.service.MemberService;
 
 //Test Data 주입
@@ -32,6 +35,7 @@ public class InitData {
         private EntityManager em;
         private final PasswordEncoder passwordEncoder;
         private final MemberService memberService;
+        private final HotelService hotelService;
 
         @Transactional
         public void init() {
@@ -42,13 +46,19 @@ public class InitData {
             createTestMember("member2", "1234", "CUSTOMER");
 
             //supplier1 생성
-            createTestMember("member3", "1234", "SUPPLIER");
+            Member member3 = createTestMember("member3", "1234", "SUPPLIER");
+            createTestHotel("hotel1", "hotel_address", "02-123-1234", member3);
+            createTestHotel("hotel2", "hotel_address", "02-123-1234", member3);
+            createTestHotel("hotel3", "hotel_address", "02-123-1234", member3);
+
+            //supplier2 생성
+            Member member4 = createTestMember("member4", "1234", "SUPPLIER");
 
             //ADMIN 생성
 
         }
 
-        public void createTestMember(String username, String password, String role) {
+        public Member createTestMember(String username, String password, String role) {
             MemberDto.Request request = new MemberDto.Request();
             request.setUsername(username);
             request.setPassword(password);
@@ -57,7 +67,17 @@ public class InitData {
             request.setEmail("testData@example.com");
             request.setRole_string(role);
 
-            memberService.createMember(request);
+            return memberService.createMember(request);
+        }
+
+        public Hotel createTestHotel(String name, String address, String phone, Member member) {
+            HotelDto.Request request = new HotelDto.Request();
+            request.setName(name);
+            request.setAddress(address);
+            request.setPhone(phone);
+            request.setMember(member);
+
+            return hotelService.createHotel(request);
         }
 
     }
